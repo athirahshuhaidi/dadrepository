2b5435e78a686525c09fd31d17c811ff4d87bd08
athirahshuhaidi <57254402+athirahshuhaidi@users.noreply.github.com>
2021-11-27 02:05:51 +0800

    Add files via upload

---
+++ lab02/src/lab02/app/RoundingApplication.java
@@ -0,0 +1,9 @@
+package lab02.app;
+
+public class RoundingApplication {
+	
+	public static void main(String[] args) {
+		System.out.println("Rounding number to the nearest decimal points");
+	}
+
+}
