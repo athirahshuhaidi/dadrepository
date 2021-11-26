fbd1f5f8ff1e85b320b56113aea11f06313476d1
athirahshuhaidi <57254402+athirahshuhaidi@users.noreply.github.com>
2021-11-27 02:35:55 +0800

    Add files via upload

---
+++ lab02/src/lab02/app/rounding/RoundingManager.java
@@ -0,0 +1,39 @@
+package lab02.rounding;
+
+import java.text.DecimalFormat;
+
+public class RoundingManager {
+	
+	public double calculatePercentage (int dividend, int divisor) {
+		// Parsing the parameter values to double
+		double percentage = ((double)dividend / (double)divisor) * 100;
+		return percentage;
+		}
+	
+	/**
+	* This method rounds the value to the requested decimal points
+	* @param value
+	* @param decimalPoints
+	* @return result in specific decimal point
+	*/
+	
+	public String roundValue(double value, int decimalPoints) {
+	
+		// Generate decimal points
+		String decimalPlaces = ".";
+		for (int counter = 0; counter < decimalPoints; counter++)
+		decimalPlaces += "0";
+		
+		// Generate empty string if requires no decimal points
+		if (decimalPoints == 0)
+		decimalPlaces = "";
+		
+		// Round the value into the specified decimal points
+		DecimalFormat formatter = new DecimalFormat("###" + decimalPlaces);
+		String result = formatter.format(value);
+		return result;
+		
+		}
+
+
+}
